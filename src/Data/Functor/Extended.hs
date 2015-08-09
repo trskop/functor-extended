52934eac66c8825a71340d50a2ade37bb4d15293
Peter Trško <peter.trsko@gmail.com>
2015-08-10 00:27:35 +0200

    Including definitions from hs-not-found
    Functions <#>, <##>, <^$^> (originally <<$>>) and <^$$^> (originally
    <<$$>>) were taken from:
    
    https://github.com/trskop/hs-not-found/blob/master/not-found/src/Data/Functor/Utils.hs

--- src/Data/Functor/Extended.hs
+++ src/Data/Functor/Extended.hs
@@ -2,57 +2,132 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 -- |
 -- Module:       $HEADER$
--- Description:  Extended module Data.Functor, from base, with additional
+-- Description:  Extended module Data.Functor with additional
 --               functions.
--- Copyright:    (c) 2015, Peter Trško
+-- Copyright:    (c) 2011, 2013-2015, Peter Trško
 -- License:      BSD3
 --
 -- Maintainer:   peter.trsko@gmail.com
 -- Stability:    experimental
 -- Portability:  CPP, NoImplicitPrelude
 --
--- Extended module "Data.Functor", from base, with additional functions.
+-- Extended module "Data.Functor" with additional functions.
 module Data.Functor.Extended
     (
     -- * Re-export of Data.Functor from base
       module Data.Functor
-
-    -- * Extra functions
 #if !MIN_VERSION_base(4,7,0)
+    -- ** Compatibility with base>=4.7
     , ($>)
 #endif
+
+    -- * Extra functions
+
+    -- ** Variations on fmap
     , (<$$>)
     , (<&>)
     , (<&$>)
+
+    -- ** Apply value to a function before fmap
+    , (<^$^>)
+    , (<^$$^>)
+
+    -- ** Apply value to a function in a functor
+    , (<#>)
+    , (<##>)
     )
   where
 
-import Data.Function (flip)
+import Data.Function (($), flip)
 import Data.Functor
 
 
 -- Function ($>) is available in base since version 4.7.0.0.
 #if !MIN_VERSION_base(4,7,0)
+-- | Flipped version of '<$'.
 ($>) :: Functor f => f a -> b -> f b
 ($>) = flip (<$)
 infixl 4 $>
 {-# INLINE ($>) #-}
 #endif
 
--- | Flipped version of ('<$>').
+-- | Flipped version of '<$>'.
 (<$$>) :: Functor f => f a -> (a -> b) -> f b
 (<$$>) = flip fmap
 infixl 4 <$$>
 {-# INLINE (<$$>) #-}
 
--- | Flipped version of ('<$>') with different fixity.
+-- | Flipped version of '<$>' with different fixity.
 (<&>) :: Functor f => f a -> (a -> b) -> f b
 (<&>) = flip fmap
 infixl 1 <&>
 {-# INLINE (<&>) #-}
 
--- | Version of ('<$>') with fixity as ('<&>') has.
+-- | Version of '<$>' with fixity as '<&>' has.
 (<&$>) :: Functor f => (a -> b) -> f a -> f b
 (<&$>) = fmap
 infixl 1 <&$>
 {-# INLINE (<&$>) #-}
+
+-- | Instead of @\\x -> f x '<$>' g x@ this function allows to write
+-- @f '<^$^>' g@.
+(<^$^>) :: Functor f => (a -> b -> c) -> (a -> f b) -> a -> f c
+(f <^$^> g) x = f x `fmap` g x
+infix 4 <^$^>
+{-# INLINE (<^$^>) #-}
+
+-- | Flipped variant of '<^$^>'.
+(<^$$^>) :: Functor f => (a -> f b) -> (a -> b -> c) -> a -> f c
+(<^$$^>) = flip (<^$^>)
+infix 4 <^$$^>
+{-# INLINE (<^$$^>) #-}
+
+-- | Apply value to a function inside a functor. Similar to using:
+--
+-- @
+-- \\f x -> f 'Control.Applicative.<*>' 'Control.Applicative.pure' x
+-- @
+--
+-- But it does require only 'Functor' and not 'Applicative' constraint.
+--
+-- === Examples
+--
+-- >>> Just (+1) <#> 2
+-- Just 3
+-- >>> [(+1), (*2)] <#> 3
+-- [4,6]
+--
+-- For @instance 'Functor' ((->) r)@ this function behaves as infix version of
+-- flip 'flip':
+--
+-- >>> (-) <#> 1 $ 2
+-- 1
+--
+-- === Implementation
+--
+-- @
+-- f '<#>' x = ('$' x) '<$>' f
+-- @
+(<#>) :: Functor f => f (a -> b) -> a -> f b
+f <#> x = ($ x) `fmap` f
+infixl 4 <#>
+{-# INLINE (<#>) #-}
+
+-- | Apply value to a function inside a functor. Similar to using:
+--
+-- @
+-- \\x f -> f 'Control.Applicative.<*>' 'Control.Applicative.pure' x
+-- @
+--
+-- But it does require only 'Functor' and not 'Control.Applicative.Applicative'
+-- constraint. Flipped version of '<#>'.
+--
+-- === Implementation
+--
+-- @
+-- x '<##>' f = ('$' x) '<$>' f
+-- @
+(<##>) :: Functor f => a -> f (a -> b) -> f b
+x <##> f = ($ x) `fmap` f
+infixl 4 <##>
+{-# INLINE (<##>) #-}
